40c914e277717941635d7f3137bb3d8dc6526242
Raghvender <raghvender002@gmail.com>
2020-06-01 20:20:23 +0530

    General Fixes

--- project_core/blog/models.py
+++ project_core/blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
