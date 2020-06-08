0d238c1938c3b1f6cb0a16f24182bbc530a5f409
Raghvender <raghvender002@gmail.com>
2020-06-08 09:47:48 +0530

    Added Dynamic Pagination & few tweaks

--- project_core/portfolio/views.py
+++ project_core/portfolio/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Project
 
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects': projects})
+
+    paginator = Paginator(projects, 6)
+    page = request.GET.get('page')
+    paged_projects = paginator.get_page(page)
+
+    context = {
+        'projects': paged_projects
+    }
+
+    return render(request, 'portfolio/home.html', context)
