16c3f56f2132d5e54d1fc49a51829debd186ad21
Raghvender <raghvender002@gmail.com>
2020-06-08 12:50:39 +0530

    Pagination added to Blog

--- project_core/blog/views.py
+++ project_core/blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Blog
 
 
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')
-    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+    paginator = Paginator(blogs, 10)
+    page = request.GET.get('page')
+    paged_blogs = paginator.get_page(page)
+
+    context = {
+        'blogs': paged_blogs
+    }
+
+    return render(request, 'blog/all_blogs.html', context)
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
